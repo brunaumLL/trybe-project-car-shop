94f7225ef8516b5ebad212f79a2aae3d7f812f2a
brunaumLL <bruno99louzada@gmail.com>
2022-09-19 17:58:58 -0300

    req 18 feito

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().int().positive().lte(2500),
+});
+
+export type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+export { MotorcycleZodSchema };
